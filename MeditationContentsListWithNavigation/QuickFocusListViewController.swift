410a806219ebdb0ee389d71122b6b3d863351654
KayAhn0126 <dkseotn123@gmail.com>
2022-09-03 20:05:01 +0900

    feat: 리스트를 클릭했을때 나올 상세화면 구현

---
+++ MeditationContentsListWithNavigation/QuickFocusListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuickFocusViewController.swift
+//  MeditationContentsListWithNavigation
+//
+//  Created by Kay on 2022/09/03.
+//
+
+import UIKit
+
+class QuickFocusListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
