6dd05f600d1286875de7477d9a5be2ea96d1bcde
KayAhn0126 <dkseotn123@gmail.com>
2022-09-03 20:36:56 +0900

    feat: 상세화면 collectionViewCell 파일 생성

---
+++ MeditationContentsListWithNavigation/QuickFocusCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  QuickFocusCollectionViewCell.swift
+//  MeditationContentsListWithNavigation
+//
+//  Created by Kay on 2022/09/03.
+//
+
+import UIKit
+
+class QuickFocusCollectionViewCell: UICollectionViewCell {
+    
+}
