7edeac5bd370d224d6161ae67e60fff27276afd3
KayAhn0126 <dkseotn123@gmail.com>
2022-09-03 20:45:37 +0900

    refactor: cell VC내 cell을 구성 메서드 구현

--- MeditationContentsListWithNavigation/QuickFocusCollectionViewCell.swift
+++ MeditationContentsListWithNavigation/QuickFocusCollectionViewCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class QuickFocusCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ data: QuickFocus) {
+        thumbnailImageView.image = UIImage(named: data.imageName)
+        titleLabel.text = data.title
+        descriptionLabel.text = data.description
+    }
 }
